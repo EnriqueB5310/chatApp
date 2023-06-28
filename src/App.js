35ca76c56b0785b9443b589a99fb36a1c13e94f7
Enrique Barrios <EnriqueB5310@gmail.com>
2023-06-27 22:28:35 -0400

    Fixed components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'; 
+import Chat from './components/Chat';
 
 firebase.initializeApp({
   apiKey: "AIzaSyBHgm32C3AEXbOkkVbNKouAlYsIUcfR8V4",
@@ -33,7 +34,7 @@ const [user] = useAuthState(auth)
       <header>
         <SignOut />
       </header>
-  {user ? <Chat /> : <SignIn />}
+  {user ? <Chat firebase={firebase} firestore={firestore} auth={auth}/> : <SignIn />}
     </div>
   );
 }
@@ -60,56 +61,7 @@ function SignOut() {
   )
 }
 
-function Chat() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('Created').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
 
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      Created: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    })
-
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  return (<>
-    <main>
-
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-      <span ref={dummy}></span>
-
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
-}
-
-function ChatMessage(props) {
-  
-  const {text, uid} = props.message
-    return  <p>{text}</p>
-   
-}
 
 export default App;
---
+++ src/components/ChatMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatMessage(props) {
+  
+    const {text, uid, photoURL} = props.message;
+    return (
+<p>{text}</p>
+  )
+}
+
+export default ChatMessage
